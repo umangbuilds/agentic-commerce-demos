cbfac5a874de02920e0903343565016466167b89
Umang <umg.gpt@gmail.com>
2026-08-24 12:58:15 +0530

    Add 8 more demos: bank, cred-metal, firstcry, landbank, myntra, planet, instamart, tijorie

---
+++ landbank-mock/_next/static/chunks/272zlod27hynj.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,83198,t=>{t.v(o=>Promise.all(["static/chunks/2fip_vv7kn34b.js"].map(o=>t.l(o))).then(()=>o(58428)))}]);
