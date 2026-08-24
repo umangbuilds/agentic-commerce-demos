66d789aaca6b256371947149eda0d382d40dfe51
Umang <umg.gpt@gmail.com>
2026-08-24 14:09:56 +0530

    Aurum Bank: wire real GPT-4o-mini via the same Cloudflare Worker proxy

---
+++ bank-mock/_next/static/chunks/22627jj7qr7uy.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,30182,t=>{t.v(e=>Promise.all(["static/chunks/26e9zi-3gsz6v.js"].map(e=>t.l(e))).then(()=>e(33165)))}]);
